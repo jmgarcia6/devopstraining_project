0d9f2a176f3cbe49a4a80c0431ba77bc930aea11
José Maria Garcia <69316376+jmgarcia6@users.noreply.github.com>
2021-11-16 09:40:13 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,3 +37,23 @@ resource "aws_security_group" "jenkins_sg" {
     Name = "Jenkins SG"
   }
 }
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["amazon"] # Canonical
+}
