08a6abf2820ff24ee87ed373957491cee433cd38
José Maria Garcia <69316376+jmgarcia6@users.noreply.github.com>
2021-11-18 17:03:54 +0000

    Create key pair

---
+++ my_key_pair.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y awscli
+
+aws ec2 create-key-pair \
+    --key-name my_key_pair \
+    --key-type rsa \
+    --query "KeyMaterial" \
+    --output text > my_key_pair.pem
+
+chmod 400 my_key_pair.pem
