52643a1aacea177d0bd4c7f52ae4aeb0bff9a6e8
José Maria Garcia <69316376+jmgarcia6@users.noreply.github.com>
2021-11-18 16:48:09 +0000

    Update outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "output_name" {
-    value = "some resource"
+output "instance_ami" {
+  description = "Instance AMI."
+  value = aws_instance.jenkins.ami
+}
+
+output "instance_arn" {
+  description = "Instance ARN."
+  value = aws_instance.jenkins.arn
 }
