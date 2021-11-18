ee8f26806c7e05df7bb32fbddb29542fd2e6dfe0
José Maria Garcia <69316376+jmgarcia6@users.noreply.github.com>
2021-11-18 19:55:33 +0000

    Update ssh_private_key_file.sh

--- ssh_private_key_file.sh
+++ ssh_private_key_file.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 sudo apt-get update
 sudo apt-get install -y awscli
 
-aws configure
-aws ec2 create-key-pair --key-name my_key_pair --key-type rsa --query "KeyMaterial" --output text > my_key_pair.pem
+rm ~/my_key_pair.pem
+aws ec2 create-key-pair --key-name my_key_pair --query "KeyMaterial" --output text > ~/my_key_pair.pem
 
-chmod 400 my_key_pair.pem
+chmod 400 ~/my_key_pair.pem
