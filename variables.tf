610c424422416e0ba91b9a22be662f559d7596eb
José Maria Garcia <69316376+jmgarcia6@users.noreply.github.com>
2021-11-18 16:52:05 +0000

    Update variables file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,34 @@
-variable "aws_region" {
-  description = "AWS region to create resources"
+variable "region" {
+  description = "AWS region"
   default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "jenkins"
+}
+
+variable "ssh_user_name" {
+  description = "SSH user name"
+  default     = "ubuntu"
+}
+
+variable "ssh_private_key_file" {
+  description = "SSH private key file"
+  default     = "~/my_key_pair.pem"
+}
+
+variable "ingress_port_rules" {
+  type    = list(number)
+  default = [8080, 22]
+}
+
+variable "key_name" {
+  description = "Key pair name"
+  default     = "my_key_pair"
 }
